cf39353f139f5e50d35f80460ac8e804f5f6eaf7
varun singhai <varun01singhai01@gmail.com>
2020-02-25 01:14:27 +0530

    removing specified item of a list

--- lambda_function.py
+++ lambda_function.py
@@ -493,6 +493,51 @@ class ChangeItemAtIndexIntentHandler(AbstractRequestHandler):
             SimpleCard(SKILL_NAME, output))
         return handler_input.response_builder.response
 
+class RemoveItemIntentHandler(AbstractRequestHandler):
+    """Handler for New Integer initialisation."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("RemoveItemIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In RemoveItemIntentHandler")
+
+        session_attributes = handler_input.attributes_manager.session_attributes
+        logger.info('SESSION ATTRIBUTES: ' + str(session_attributes))
+        
+        first_variable = get_slot_data(handler_input, 'first_variable', logger=logger)['value']
+        second_variable = get_slot_data(handler_input, 'second_variable', logger=logger)['value']
+        
+        output=""
+                 
+        if first_variable is None:
+            logger.debug('{first_variable} not provided')
+        if second_variable is None:
+            logger.debug('{second_variable} not provided')
+
+        if (first_variable is None) and (second_variable is None) :
+            output += 'Both List name and variable name to be removed not provided.'
+        elif first_variable is None:
+            output += ' List name not provided.'
+        elif second_variable is None:
+            output += ' variable which you want to remove not provided.'
+        else:
+            indent = get_indent(handler_input)
+            script_line = indent+ f"{first_variable}.remove({second_variable})"
+            try:
+                session_attributes['current_script_code'] += '\n'
+                session_attributes['current_script_code'] += script_line
+            except KeyError:
+                session_attributes['current_script_code'] = script_line
+                
+            output = session_attributes['current_script_code']
+
+        handler_input.response_builder.speak(output).set_card(
+            SimpleCard(SKILL_NAME, output))
+        return handler_input.response_builder.response
+
 
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
@@ -604,6 +649,7 @@ sb.add_request_handler(SessionEndedRequestHandler())
 sb.add_request_handler(NewIfBlockIntentHandler())
 sb.add_request_handler(NewElIfBlockIntentHandler())
 sb.add_request_handler(ChangeItemAtIndexIntentHandler())
+sb.add_request_handler(RemoveItemIntentHandler())
 
 # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
 # sb.add_request_handler(IntentReflectorHandler())
