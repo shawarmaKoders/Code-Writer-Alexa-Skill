341b0692f77273eed523fa59bca9dbe240cab34f
varun singhai <varun01singhai01@gmail.com>
2020-02-26 23:46:13 +0530

    changing item at particular index

--- lambda_function.py
+++ lambda_function.py
@@ -332,6 +332,51 @@ class ForLoopIntentHandler(AbstractRequestHandler):
             SimpleCard(SKILL_NAME, output))
         return handler_input.response_builder.response
 
+class ChangeItemAtIndexIntentHandler(AbstractRequestHandler):
+    """Handler for changing item at index of list."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("ChangeItemAtIndexIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In ChangeItemAtIndexIntentHandler")
+
+        session_attributes = handler_input.attributes_manager.session_attributes
+        logger.info('SESSION ATTRIBUTES: ' + str(session_attributes))
+        
+        first_variable = get_slot_data(handler_input, 'first_variable', logger=logger)['value']
+        second_variable = get_slot_data(handler_input, 'second_variable', logger=logger)['value']
+        index_value = get_slot_data(handler_input, 'index_value', logger=logger)['value']
+        output=""
+        if index_value is None:
+            logger.debug('{index_value} not provided')           
+        if first_variable is None:
+            logger.debug('{first_variable} not provided')
+        if second_variable is None:
+            logger.debug('{second_variable} not provided')
+
+        if (first_variable is None) and (second_variable is None) and (index_value is None):
+            output += 'All three : List name, index value and variable name not provided.'
+        elif first_variable is None:
+            output += ' List name not provided.'
+        elif second_variable is None:
+            output += ' variable which you want to add not provided.'
+        else:
+            script_line = f"{first_variable}[{index_value}]={second_variable}"
+            try:
+                session_attributes['current_script_code'] += '\n'
+                session_attributes['current_script_code'] += script_line
+            except KeyError:
+                session_attributes['current_script_code'] = script_line
+                
+            output = session_attributes['current_script_code']
+
+        handler_input.response_builder.speak(output).set_card(
+            SimpleCard(SKILL_NAME, output))
+        return handler_input.response_builder.response
+
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
@@ -515,6 +560,7 @@ sb.add_request_handler(ListAppendIntentHandler())
 sb.add_request_handler(ForLoopIntentHandler())
 sb.add_request_handler(CreateWhileLoopIntentHandler())
 sb.add_request_handler(NewStringIntentHandler())
+sb.add_request_handler(ChangeItemAtIndexIntentHandler())
 
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
